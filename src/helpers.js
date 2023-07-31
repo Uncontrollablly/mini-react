b29f67f0f669f707a214fc85bb64f2878fb99e64
wangyuqi <494849261@qq.com>
2023-07-31 22:57:57 +0800

    feat: implement step 1 and 2

---
+++ src/helpers.js
@@ -0,0 +1,3 @@
+const isProperty = (key) => key !== "children";
+
+export { isProperty };
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Didact } from "./didact";
+
+/** @jsx Didact.createElement */
+const element = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Didact</h2>
+  </div>
+);
+
+const container = document.getElementById("root");
+
+Didact.render(element, container);
